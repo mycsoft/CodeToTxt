fa54a141c1e22099d9541ab57adbfe183dac5bb2
mycsoft <mycsoft@qq.com>
2014-06-17 13:10:39 +0800

    加入扫描进度信息显示.

--- src/com/myc/ctt/CodeToTxt.java
+++ src/com/myc/ctt/CodeToTxt.java
@@ -27,6 +27,7 @@ public class CodeToTxt {
             showHelp();
             return;
         }
+        long t = System.currentTimeMillis();
 
         try {
             System.out.println("开始扫描.");
@@ -34,19 +35,26 @@ public class CodeToTxt {
             String rootPath = args.length == 0 ? null : args[0];     //扫描根路径
             File rootDir;   //扫描的根目录.
             if (rootPath == null) {
+                System.out.println("请传入要扫描目录.");
+                showHelp();
+                return;
                 //以当前目录为根目录.
-                rootPath = CodeToTxt.class.getResource(".").getFile();
+//                rootPath = CodeToTxt.class.getResource(".").getFile();
             }
-            System.out.println("扫描路径:"+rootPath);
+            System.out.println("扫描路径:" + rootPath);
             rootDir = new File(rootPath);
 //        File outFile = File.createTempFile("ctt", ".txt");
-            //TODO 扫描文件
+            //扫描目录
             File outFile = scanDir(rootDir);
-            //TODO 输出文件.
+            //输出文件.
             File dest = new File(rootDir, outFile.getName());
             outFile.renameTo(dest);
-            System.out.println("输出到:"+dest.getAbsolutePath());
-            System.out.println("扫描完成.");
+            System.out.println("输出到:" + dest.getAbsolutePath());
+            //计算用时
+            t = System.currentTimeMillis() - t;
+            t = t < 1000 ? 1 : t / 1000;
+
+            System.out.println("扫描完成.共用时" + (t > 60 ? (t > 360 ? (t / 360f + "小时") : t / 60f + "分钟") : t + "秒"));
         } catch (Exception e) {
             System.out.println("扫描失败.请查看日志.");
             e.printStackTrace();
@@ -57,7 +65,7 @@ public class CodeToTxt {
      * 显示帮助.
      */
     private static void showHelp() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("用法:CodeToTxt [scan dir path]");
     }
 
     /**
@@ -71,7 +79,7 @@ public class CodeToTxt {
 
         FileOutputStream out = new FileOutputStream(outFile);
         try {
-            System.out.println(rootDir.getAbsolutePath());
+
             scanDir(rootDir, out);
 
         } finally {
@@ -83,10 +91,12 @@ public class CodeToTxt {
 
     /**
      * 扫描目录.
+     *
      * @param dir
-     * @param out 
+     * @param out
      */
     private static void scanDir(File dir, FileOutputStream out) throws FileNotFoundException, IOException {
+        System.out.println(dir.getAbsolutePath());
         File[] files = dir.listFiles(new FileFilter() {
 
             @Override
@@ -105,13 +115,14 @@ public class CodeToTxt {
     }
 
     private static void scanFile(File file, FileOutputStream out) throws FileNotFoundException, IOException {
+        System.out.println("\t" + file.getAbsolutePath());
         FileInputStream in = new FileInputStream(file);
         try {
             byte[] b = new byte[1024];
-            while(in.read(b)> 0){
+            while (in.read(b) > 0) {
                 out.write(b);
             }
-        } finally{
+        } finally {
             in.close();
         }
     }
